366336b60a49f9346cccbd0fc9314e83ceb7a41c
Carari Victor-Lucian <cararivictor2608@outlook.com>
2024-03-19 19:18:36 +0200

    adugare proiect

---
+++ alobazabaza/Form1.Designer.cs
@@ -0,0 +1,381 @@
+﻿namespace alobazabaza
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.admitereDataGridView = new System.Windows.Forms.DataGridView();
+            this.button_pb0 = new System.Windows.Forms.Button();
+            this.button_pb1 = new System.Windows.Forms.Button();
+            this.button_pb2 = new System.Windows.Forms.Button();
+            this.button_pb3 = new System.Windows.Forms.Button();
+            this.button_pb4_1 = new System.Windows.Forms.Button();
+            this.button_pb4_2 = new System.Windows.Forms.Button();
+            this.button_pb5 = new System.Windows.Forms.Button();
+            this.button_pb6 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button_pb8 = new System.Windows.Forms.Button();
+            this.button_pb7 = new System.Windows.Forms.Button();
+            this.button_pb10_1 = new System.Windows.Forms.Button();
+            this.button_pb9 = new System.Windows.Forms.Button();
+            this.button_pb10_2 = new System.Windows.Forms.Button();
+            this.button_pb11 = new System.Windows.Forms.Button();
+            this.button_12_2 = new System.Windows.Forms.Button();
+            this.button_12_1 = new System.Windows.Forms.Button();
+            this.button_13_2 = new System.Windows.Forms.Button();
+            this.button_13_1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.admitereDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // admitereDataGridView
+            // 
+            this.admitereDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.admitereDataGridView.Location = new System.Drawing.Point(13, 13);
+            this.admitereDataGridView.Name = "admitereDataGridView";
+            this.admitereDataGridView.Size = new System.Drawing.Size(685, 360);
+            this.admitereDataGridView.TabIndex = 0;
+            // 
+            // button_pb0
+            // 
+            this.button_pb0.Location = new System.Drawing.Point(716, 13);
+            this.button_pb0.Name = "button_pb0";
+            this.button_pb0.Size = new System.Drawing.Size(75, 23);
+            this.button_pb0.TabIndex = 1;
+            this.button_pb0.Text = "Pb 0";
+            this.button_pb0.UseVisualStyleBackColor = true;
+            this.button_pb0.Click += new System.EventHandler(this.button_pb0_Click);
+            // 
+            // button_pb1
+            // 
+            this.button_pb1.Location = new System.Drawing.Point(797, 13);
+            this.button_pb1.Name = "button_pb1";
+            this.button_pb1.Size = new System.Drawing.Size(75, 23);
+            this.button_pb1.TabIndex = 2;
+            this.button_pb1.Text = "Pb 1";
+            this.button_pb1.UseVisualStyleBackColor = true;
+            // 
+            // button_pb2
+            // 
+            this.button_pb2.Location = new System.Drawing.Point(716, 42);
+            this.button_pb2.Name = "button_pb2";
+            this.button_pb2.Size = new System.Drawing.Size(75, 23);
+            this.button_pb2.TabIndex = 3;
+            this.button_pb2.Text = "Pb 2";
+            this.button_pb2.UseVisualStyleBackColor = true;
+            // 
+            // button_pb3
+            // 
+            this.button_pb3.Location = new System.Drawing.Point(797, 42);
+            this.button_pb3.Name = "button_pb3";
+            this.button_pb3.Size = new System.Drawing.Size(75, 23);
+            this.button_pb3.TabIndex = 4;
+            this.button_pb3.Text = "Pb 3";
+            this.button_pb3.UseVisualStyleBackColor = true;
+            // 
+            // button_pb4_1
+            // 
+            this.button_pb4_1.Location = new System.Drawing.Point(716, 71);
+            this.button_pb4_1.Name = "button_pb4_1";
+            this.button_pb4_1.Size = new System.Drawing.Size(75, 23);
+            this.button_pb4_1.TabIndex = 5;
+            this.button_pb4_1.Text = "Pb 4.1";
+            this.button_pb4_1.UseVisualStyleBackColor = true;
+            this.button_pb4_1.Click += new System.EventHandler(this.button_pb4_1_Click);
+            // 
+            // button_pb4_2
+            // 
+            this.button_pb4_2.Location = new System.Drawing.Point(797, 71);
+            this.button_pb4_2.Name = "button_pb4_2";
+            this.button_pb4_2.Size = new System.Drawing.Size(75, 23);
+            this.button_pb4_2.TabIndex = 6;
+            this.button_pb4_2.Text = "Pb 4.2";
+            this.button_pb4_2.UseVisualStyleBackColor = true;
+            this.button_pb4_2.Click += new System.EventHandler(this.button_pb4_2_Click);
+            // 
+            // button_pb5
+            // 
+            this.button_pb5.Location = new System.Drawing.Point(716, 100);
+            this.button_pb5.Name = "button_pb5";
+            this.button_pb5.Size = new System.Drawing.Size(75, 23);
+            this.button_pb5.TabIndex = 7;
+            this.button_pb5.Text = "Pb 5";
+            this.button_pb5.UseVisualStyleBackColor = true;
+            this.button_pb5.Click += new System.EventHandler(this.button_pb5_Click);
+            // 
+            // button_pb6
+            // 
+            this.button_pb6.Location = new System.Drawing.Point(797, 100);
+            this.button_pb6.Name = "button_pb6";
+            this.button_pb6.Size = new System.Drawing.Size(75, 23);
+            this.button_pb6.TabIndex = 8;
+            this.button_pb6.Text = "Pb 6";
+            this.button_pb6.UseVisualStyleBackColor = true;
+            this.button_pb6.Click += new System.EventHandler(this.button_pb6_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(716, 141);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "1.00 - 5.00";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(780, 138);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 10;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(780, 164);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(716, 167);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "5.00 - 7.00";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(780, 190);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(716, 193);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "7.00 - 9.00";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(780, 216);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 16;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(716, 219);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "9.00 - 10";
+            // 
+            // button_pb8
+            // 
+            this.button_pb8.Location = new System.Drawing.Point(797, 242);
+            this.button_pb8.Name = "button_pb8";
+            this.button_pb8.Size = new System.Drawing.Size(75, 23);
+            this.button_pb8.TabIndex = 18;
+            this.button_pb8.Text = "Pb 8";
+            this.button_pb8.UseVisualStyleBackColor = true;
+            // 
+            // button_pb7
+            // 
+            this.button_pb7.Location = new System.Drawing.Point(716, 242);
+            this.button_pb7.Name = "button_pb7";
+            this.button_pb7.Size = new System.Drawing.Size(75, 23);
+            this.button_pb7.TabIndex = 17;
+            this.button_pb7.Text = "Pb 7";
+            this.button_pb7.UseVisualStyleBackColor = true;
+            this.button_pb7.Click += new System.EventHandler(this.button_pb7_Click);
+            // 
+            // button_pb10_1
+            // 
+            this.button_pb10_1.Location = new System.Drawing.Point(797, 271);
+            this.button_pb10_1.Name = "button_pb10_1";
+            this.button_pb10_1.Size = new System.Drawing.Size(75, 23);
+            this.button_pb10_1.TabIndex = 20;
+            this.button_pb10_1.Text = "Pb 10.1";
+            this.button_pb10_1.UseVisualStyleBackColor = true;
+            this.button_pb10_1.Click += new System.EventHandler(this.button_pb10_1_Click);
+            // 
+            // button_pb9
+            // 
+            this.button_pb9.Location = new System.Drawing.Point(716, 271);
+            this.button_pb9.Name = "button_pb9";
+            this.button_pb9.Size = new System.Drawing.Size(75, 23);
+            this.button_pb9.TabIndex = 19;
+            this.button_pb9.Text = "Pb 9";
+            this.button_pb9.UseVisualStyleBackColor = true;
+            this.button_pb9.Click += new System.EventHandler(this.button_pb9_Click);
+            // 
+            // button_pb10_2
+            // 
+            this.button_pb10_2.Location = new System.Drawing.Point(716, 300);
+            this.button_pb10_2.Name = "button_pb10_2";
+            this.button_pb10_2.Size = new System.Drawing.Size(75, 23);
+            this.button_pb10_2.TabIndex = 21;
+            this.button_pb10_2.Text = "Pb 10.2";
+            this.button_pb10_2.UseVisualStyleBackColor = true;
+            this.button_pb10_2.Click += new System.EventHandler(this.button_pb10_2_Click);
+            // 
+            // button_pb11
+            // 
+            this.button_pb11.Location = new System.Drawing.Point(797, 300);
+            this.button_pb11.Name = "button_pb11";
+            this.button_pb11.Size = new System.Drawing.Size(75, 23);
+            this.button_pb11.TabIndex = 22;
+            this.button_pb11.Text = "Pb 11";
+            this.button_pb11.UseVisualStyleBackColor = true;
+            this.button_pb11.Click += new System.EventHandler(this.button_pb11_Click);
+            // 
+            // button_12_2
+            // 
+            this.button_12_2.Location = new System.Drawing.Point(797, 329);
+            this.button_12_2.Name = "button_12_2";
+            this.button_12_2.Size = new System.Drawing.Size(75, 23);
+            this.button_12_2.TabIndex = 24;
+            this.button_12_2.Text = "Pb 12.2";
+            this.button_12_2.UseVisualStyleBackColor = true;
+            this.button_12_2.Click += new System.EventHandler(this.button_12_2_Click);
+            // 
+            // button_12_1
+            // 
+            this.button_12_1.Location = new System.Drawing.Point(716, 329);
+            this.button_12_1.Name = "button_12_1";
+            this.button_12_1.Size = new System.Drawing.Size(75, 23);
+            this.button_12_1.TabIndex = 23;
+            this.button_12_1.Text = "Pb 12.1";
+            this.button_12_1.UseVisualStyleBackColor = true;
+            this.button_12_1.Click += new System.EventHandler(this.button_12_1_Click);
+            // 
+            // button_13_2
+            // 
+            this.button_13_2.Location = new System.Drawing.Point(797, 358);
+            this.button_13_2.Name = "button_13_2";
+            this.button_13_2.Size = new System.Drawing.Size(75, 23);
+            this.button_13_2.TabIndex = 26;
+            this.button_13_2.Text = "Pb 13.2";
+            this.button_13_2.UseVisualStyleBackColor = true;
+            this.button_13_2.Click += new System.EventHandler(this.button_13_2_Click);
+            // 
+            // button_13_1
+            // 
+            this.button_13_1.Location = new System.Drawing.Point(716, 358);
+            this.button_13_1.Name = "button_13_1";
+            this.button_13_1.Size = new System.Drawing.Size(75, 23);
+            this.button_13_1.TabIndex = 25;
+            this.button_13_1.Text = "Pb 13.1";
+            this.button_13_1.UseVisualStyleBackColor = true;
+            this.button_13_1.Click += new System.EventHandler(this.button_13_1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 387);
+            this.Controls.Add(this.button_13_2);
+            this.Controls.Add(this.button_13_1);
+            this.Controls.Add(this.button_12_2);
+            this.Controls.Add(this.button_12_1);
+            this.Controls.Add(this.button_pb11);
+            this.Controls.Add(this.button_pb10_2);
+            this.Controls.Add(this.button_pb10_1);
+            this.Controls.Add(this.button_pb9);
+            this.Controls.Add(this.button_pb8);
+            this.Controls.Add(this.button_pb7);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button_pb6);
+            this.Controls.Add(this.button_pb5);
+            this.Controls.Add(this.button_pb4_2);
+            this.Controls.Add(this.button_pb4_1);
+            this.Controls.Add(this.button_pb3);
+            this.Controls.Add(this.button_pb2);
+            this.Controls.Add(this.button_pb1);
+            this.Controls.Add(this.button_pb0);
+            this.Controls.Add(this.admitereDataGridView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.admitereDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView admitereDataGridView;
+        private System.Windows.Forms.Button button_pb0;
+        private System.Windows.Forms.Button button_pb1;
+        private System.Windows.Forms.Button button_pb2;
+        private System.Windows.Forms.Button button_pb3;
+        private System.Windows.Forms.Button button_pb4_1;
+        private System.Windows.Forms.Button button_pb4_2;
+        private System.Windows.Forms.Button button_pb5;
+        private System.Windows.Forms.Button button_pb6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button_pb8;
+        private System.Windows.Forms.Button button_pb7;
+        private System.Windows.Forms.Button button_pb10_1;
+        private System.Windows.Forms.Button button_pb9;
+        private System.Windows.Forms.Button button_pb10_2;
+        private System.Windows.Forms.Button button_pb11;
+        private System.Windows.Forms.Button button_12_2;
+        private System.Windows.Forms.Button button_12_1;
+        private System.Windows.Forms.Button button_13_2;
+        private System.Windows.Forms.Button button_13_1;
+    }
+}
+
---
+++ alobazabaza/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace alobazabaza
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection myConnection;
+        SqlDataAdapter myAdapter;
+        DataTable myTable;
+        SqlCommand q;
+
+        public void openConnection()
+        {
+            string myConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|Admitere.mdf;Integrated Security=True;Connect Timeout=30";
+            myConnection = new SqlConnection(myConnectionString);
+            myConnection.Open();
+        }
+
+        private void select(string qs)
+        {
+            myAdapter = new SqlDataAdapter();
+            myTable = new DataTable();
+            myTable.Clear();
+            myTable.Columns.Clear();
+            admitereDataGridView.Columns.Clear();
+            q = new SqlCommand(qs, myConnection);
+            myAdapter.SelectCommand = q;
+            myAdapter.Fill(myTable);
+            admitereDataGridView.DataSource = myTable;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            openConnection();
+
+            q = new SqlCommand("update Admitere set rezultat = ' ', media = 0", myConnection);
+            q.ExecuteNonQuery();
+
+            select("select * from Admitere");
+
+        }
+
+        private void button_pb0_Click(object sender, EventArgs e)
+        {
+            q = new SqlCommand("update Admitere set rezultat = 'RESPINS', media = (proba1 + proba2 - 0.01) / 2", myConnection);
+            q.ExecuteNonQuery();
+
+            q = new SqlCommand("update Admitere set rezultat = 'ADMIS' where id in ( select top 20 id from Admitere where (proba1>=5) and (proba2>=5) order by media desc)", myConnection);
+            q.ExecuteNonQuery();
+            select("select * from Admitere");
+        }
+
+        private void button_pb4_1_Click(object sender, EventArgs e)
+        {
+            select("select nume, prenume, proba1, rezultat from Admitere order by proba1 desc");
+        }
+
+        private void button_pb4_2_Click(object sender, EventArgs e)
+        {
+            select("select nume, prenume, proba2, rezultat from Admitere order by proba2 desc");
+        }
+
+        private void button_pb5_Click(object sender, EventArgs e)
+        {
+            select("select nume, prenume, media, rezultat from Admitere order by nume, prenume");
+        }
+
+        private void button_pb6_Click(object sender, EventArgs e)
+        {
+            string s = ("select count(*) from Admitere");
+            q = new SqlCommand(s, myConnection);
+            int nrtot = Convert.ToInt32(q.ExecuteScalar());
+
+            s = ("select count(*) from Admitere where media <= 5");
+            q = new SqlCommand(s, myConnection);
+            int nr5 = Convert.ToInt32(q.ExecuteScalar());
+            textBox1.Text = (nr5 * 100) / nrtot + "%";
+
+            s = ("select count(*) from Admitere where media > 5 and media <= 7");
+            q = new SqlCommand(s, myConnection);
+            int nr7 = Convert.ToInt32(q.ExecuteScalar());
+            textBox2.Text = (nr7 * 100) / nrtot + "%";
+
+            s = ("select count(*) from Admitere where media > 7 and media <= 9");
+            q = new SqlCommand(s, myConnection);
+            int nr9 = Convert.ToInt32(q.ExecuteScalar());
+            textBox3.Text = (nr9 * 100) / nrtot + "%";
+
+            s = ("select count(*) from Admitere where media > 9");
+            q = new SqlCommand(s, myConnection);
+            int nr10 = Convert.ToInt32(q.ExecuteScalar());
+            textBox4.Text = (nr10 * 100) / nrtot + "%";
+        }
+
+        private void button_pb7_Click(object sender, EventArgs e)
+        {
+            select("select nume, prenume, media, rezultat, oras from Admitere order by oras, nume, prenume");
+        }
+
+        private void button_pb9_Click(object sender, EventArgs e)
+        {
+            select("select nume, prenume, media, oras from Admitere where media = (select max(media) from Admitere as x where x.oras = Admitere.oras) order by oras");
+        }
+
+        private void button_pb10_1_Click(object sender, EventArgs e)
+        {
+            select("select id, nume, prenume, sex, proba1, proba2, media, datan, rezultat from Admitere where oras = 'Cluj' and rezultat = 'ADMIS' order by media desc, nume, prenume");
+        }
+
+        private void button_pb10_2_Click(object sender, EventArgs e)
+        {
+            select("select id, nume, prenume, sex, proba1, proba2, media, datan, rezultat from Admitere where oras = 'Cluj' and rezultat = 'RESPINS' order by media desc, nume, prenume");
+        }
+
+        private void button_pb11_Click(object sender, EventArgs e)
+        {
+            select("select top 4 nume, prenume, oras, media from Admitere where oras != 'Brasov' order by media desc, proba1");
+        }
+
+        private void button_12_1_Click(object sender, EventArgs e)
+        {
+            select("select top 2 nume, prenume, oras, media from Admitere where sex = 'M' and oras !='Brasov' and rezultat = 'ADMIS' order by media desc, proba1");
+        }
+
+        private void button_12_2_Click(object sender, EventArgs e)
+        {
+            select("select top 3 nume, prenume, oras, media from Admitere where sex = 'F' and oras !='Brasov' and rezultat = 'ADMIS' order by media desc, proba1");
+        }
+
+        private void button_13_1_Click(object sender, EventArgs e)
+        {
+            select("select nume, prenume, media from Admitere where media>=9.75 and media<=10 order by nume, prenume");
+        }
+
+        private void button_13_2_Click(object sender, EventArgs e)
+        {
+            select("select nume, prenume, media from Admitere where media>=8.50 and media<=9.74 order by nume, prenume");
+        }
+    }
+}
