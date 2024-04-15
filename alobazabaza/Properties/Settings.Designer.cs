78ccc41b781b8b8e024dbf5b76220d43f01c0d09
Carari Victor-Lucian <cararivictor2608@outlook.com>
2024-04-15 17:01:39 +0300

    .

--- alobazabaza/Properties/Settings.Designer.cs
+++ alobazabaza/Properties/Settings.Designer.cs
@@ -27,7 +27,7 @@ namespace alobazabaza.Properties {
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
         [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\bin\\Debug\\Adm" +
-            "itere.mdf;Integrated Security=True;Connect Timeout=30;Encrypt=True")]
+            "itere.mdf;Integrated Security=True;Connect Timeout=30")]
         public string AdmitereConnectionString {
             get {
                 return ((string)(this["AdmitereConnectionString"]));
